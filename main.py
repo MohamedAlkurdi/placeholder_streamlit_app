b80e494b14b15778f1e7a1deef2a152ad039df5d
MohamedAlkurdi <muhammed.alkurdi.he@gmail.com>
2025-01-19 15:15:11 +0300

    data source modification

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-dataloc = '../../graduation_project/the_project/Classification/output/regions/north_america_australia/genral_labeled_data_with_relative_traffic_rates/Entertainment/USA_with_relative_traffic_rates.csv'
+dataloc = './USA_with_relative_traffic_rates.csv'
 data = pd.read_csv(dataloc)
 
 st.write("# Simple placeholder streamlit app")
